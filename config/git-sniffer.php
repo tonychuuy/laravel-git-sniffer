9109efe7cbe8530da7538c56cb60c7062e51a338
Jesus Aviles <avirdz@gmail.com>
2016-05-24 09:07:55 -0700

    added composer scripts to copy the git hooks

--- config/git-sniffer.php
+++ config/git-sniffer.php
@@ -1,21 +1,26 @@
 <?php
 
 return array(
-
-    /*
-     |--------------------------------------------------------------------------
-     | Laravel Git Sniffer Settings
-     |--------------------------------------------------------------------------
-     |
-     */
+    // run the commands only in this environment
     'env' => 'local',
+
+    // full path for phpcs bin
     'phpcs_bin' => './vendor/bin/phpcs',
+
+    // code standard to verify
     'standard' => 'PSR2',
 
+    // file encoding
     'encoding' => 'utf-8',
+
+    // valid file extensions to verify
     'extensions' => [
         'php'
     ],
+
+    // ignore list
     'ignore' => [],
+
+    // temp dir to staged files
     'temp' => '.tmp_staging',
 );
